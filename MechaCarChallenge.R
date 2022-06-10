fe94239f710410af6fd434bbee9cc5fbf0536347
lsulecki <leiana.sulecki@gmail.com>
2022-06-10 17:31:12 -0400

    Deliverable 2 submission with images, updated R Script

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,6 +1,20 @@
+library(tidyverse)
+library(dplyr)
+
+#Deliverable 1:
 mechacar_table <- read.csv('MechaCar_mpg.csv', check.names = F, stringsAsFactors = F) # import MechaCar dataset
 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_table) #generate multiple linear regression model
 
 summary(lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
              ground_clearance + AWD, data = mechacar_table)) #generate summary statistics
+
+
+#Deliverable 2:
+suspension_coil_table <- read.csv('Suspension_Coil.csv', check.names = F, stringsAsFactors = F) #import suspension coil data
+
+total_summary <- suspension_coil_table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),
+                                                          Std_Dev=sd(PSI)) 
+
+lot_summary <- suspension_coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), 
+                                                          Variance=var(PSI), Std_Dev=sd(PSI), .groups = 'keep')
--- README.md
+++ README.md
@@ -5,3 +5,13 @@
 - Which variables/coefficients provided a non-random amount of variance to the mpg values in the dataset?
 - Is the slope of the linear model considered to be zero? Why or why not?
 - Does this linear model predict mpg of the MechaCar prototypes effectively? Why or why not?
+(add image of multiple_linear_regresh)
+
+## Summary Statistics on Suspension Coils
+
+(add image of total summary/lot summary)
+
+- The design specifications for the MechaCar suspension coils dictate that the variance of the suspension coils 
+must not exceed 100 pounds per square inch. Does the current manufacturing data meet this design specification for 
+all manufacturing lots in total and each lot individually? Why or why not?
+
